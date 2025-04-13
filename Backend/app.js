f9d008548311dfe28b28bf7b4bdffbb9e05e7733
Pradeep-003 <pradeep50953@gmail.com>
2025-04-13 12:42:32 +0530

    Basic setup

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const connectToDb = require("./db/db");
 const app = express();
+
+connectToDb();
 
 app.use(cors());
 
